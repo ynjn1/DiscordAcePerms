6b8956508ef5bf4c7e8d1a2c3515fca17be85b82
ynjn1 <96897966+ynjn1@users.noreply.github.com>
2021-12-30 22:04:24 -0500

    fixed your shit
    your welcome

--- config.lua
+++ config.lua
@@ -3,11 +3,11 @@ Config = {
 	Discord_Link = 'https://discord.gg/',
 	Website_Link = 'https://badger.store/',
 	roleList = {
-		{778070857964716033, "group.member"},
-		{778084605198204948, "group.booster"},
-		{778078566726369360, "group.certified"},
-		{778076976345907250, "group.subscriber"},
-		{778074943824592916, "group.moderator"}, 
-		{778075273065136149, "group.owner"}, 
+		{'778070857964716033', "group.member"},
+		{'778084605198204948', "group.booster"},
+		{'778078566726369360', "group.certified"},
+		{'778076976345907250', "group.subscriber"},
+		{'778074943824592916', "group.moderator"}, 
+		{'778075273065136149', "group.owner"}, 
 	},
 }
